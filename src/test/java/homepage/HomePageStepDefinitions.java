243e4d0d3afc1ead8327f07975e627837691a12a
ViktorTakacs <viktor.takacs@theitsolutions.io>
2022-05-10 22:14:29 +0200

    - Implemented a test for adding items to the cart - Added jUnit vintage dependency for running batch tests

--- src/test/java/homepage/HomePageStepDefinitions.java
+++ src/test/java/homepage/HomePageStepDefinitions.java
@@ -1,19 +1,22 @@
 package homepage;
 
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import homepage.HomePage;
-import io.cucumber.java.After;
+import java.time.Duration;
+import java.util.regex.Pattern;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import lombok.extern.slf4j.Slf4j;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-@Slf4j
 public class HomePageStepDefinitions {
 
     ChromeDriver webDriver = new ChromeDriver();
@@ -21,13 +24,6 @@ public class HomePageStepDefinitions {
 
     static {
         WebDriverManager.chromedriver().setup();
-    }
-
-    @After
-    void teardown() {
-        if (webDriver != null) {
-            webDriver.quit();
-        }
     }
 
     @Given("the home page is opened")
@@ -49,6 +45,41 @@ public class HomePageStepDefinitions {
             fail();
         }
 
-        teardown();
+        webDriver.close();
+    }
+
+    @When("and item is added to the cart {string} times")
+    public void andItemIsAddedToTheCartNumberOfClicksTimes(String numberOfClicksString) {
+        WebElement addToCartButton = webDriver
+            .findElement(By.cssSelector("#content div:nth-of-type(2) div:nth-of-type(1) div.product-thumb.transition div:nth-of-type(3) button:nth-of-type(1)"));
+
+        int numberOfClicks = Integer.parseInt(numberOfClicksString);
+
+        for (int i = 0; i < numberOfClicks; i++) {
+            addToCartButton.click();
+            Awaitility.waitAtMost(Duration.ofMillis(200)); //For consistency, sometimes the tests would fail if this is not here
+        }
+    }
+
+    @Then("I have {string} items in my cart")
+    public void iHaveNumberOfItemsItemsInMyCart(String numberOfItemsString) {
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.textMatches(By.cssSelector("#cart-total"), Pattern.compile(".*item.*")));
+        WebElement cartButton = webDriver.findElement(By.cssSelector("#cart-total"));
+
+        String actualNumberOfItems =
+            cartButton.getText()
+                .trim()
+                .split(" ")[0];
+
+        int expected = Integer.parseInt(numberOfItemsString);
+        int actual = Integer.parseInt(actualNumberOfItems);
+
+        if (!(expected == actual)) {
+            fail();
+        }
+
+        webDriver.close();
     }
 }
---
+++ src/test/resources/add_items_to_cart.feature
@@ -0,0 +1,15 @@
+Feature: Add items to cart
+
+  Background:
+    Given the home page is opened
+
+    Scenario Outline:
+      When and item is added to the cart '<numberOfClicks>' times
+      Then I have '<numberOfItems>' items in my cart
+
+      Examples:
+        | numberOfClicks | numberOfItems |
+        | 0              | 0             |
+        | 1              | 1             |
+        | 2              | 2             |
+        | 10             | 10            |
